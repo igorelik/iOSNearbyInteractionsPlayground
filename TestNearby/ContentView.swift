9518b7239d694db024411802403f6c9271b46877
igorelik <gorelik.igor@gmail.com>
2022-06-21 09:38:08 +1000

    Initial Commit

---
+++ TestNearby/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TestNearby
+//
+//  Created by Igor Gorelik on 21/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestNearby/TestNearbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNearbyApp.swift
+//  TestNearby
+//
+//  Created by Igor Gorelik on 21/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestNearbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
