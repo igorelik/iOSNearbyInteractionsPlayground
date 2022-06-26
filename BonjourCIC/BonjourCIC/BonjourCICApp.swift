ec050b048f7cdc1eeeadcedb38ec5cc5ddb132f9
igorelik <gorelik.igor@gmail.com>
2022-06-26 15:59:56 +1000

    Initial version of TicTacToe based app (https://github.com/cntrump/Tic-Tac-Toe)

---
+++ BonjourCIC/BonjourCIC/BonjourCICApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonjourCICApp.swift
+//  BonjourCIC
+//
+//  Created by Igor Gorelik on 26/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct BonjourCICApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
